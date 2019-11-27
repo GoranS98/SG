ff4232e2ea0b8ada635a57cee982a8364ead530c
GoranS98 <tirkizastisjaj0519>
2019-11-27 20:02:27 +0100

    jquery

---
+++ jQuery domaci/zadatak-jq/skriptjq.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("body").css(( "background-color" : "burlywood" , "fontfamily" : "tahoma"));     
+                 
+                 
+                 
+                 
+    
+    });
+                  
